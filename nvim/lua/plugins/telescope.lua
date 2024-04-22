0a74e9a634c5f2c47b3a141d922d71d8d9fcafac
Paul van de Ven <paul@get-protocol.io>
2024-04-22 21:11:56 +0200

    find_files telescope hidden files tweaks

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -7,13 +7,48 @@ return {
     tag = "0.1.6",
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
-      require("telescope").setup({
+      local telescope = require("telescope")
+      local telescopeConfig = require("telescope.config")
+      
+      -- Clone the default Telescope configuration
+      local vimgrep_arguments = { unpack(telescopeConfig.values.vimgrep_arguments) }
+      
+      -- search in hidden/dot files.
+      table.insert(vimgrep_arguments, "--hidden")
+      -- don't search in the `.git` directory.
+      table.insert(vimgrep_arguments, "--glob")
+      table.insert(vimgrep_arguments, "!**/.git/*")
+
+      telescope.setup({
+        defaults = {
+          -- `hidden = true` is not supported in text grep commands.
+          vimgrep_arguments = vimgrep_arguments,
+        },
+        file_ignore_patterns = {
+          ".git/*",
+          "node_modules/*",
+          "bower_components/*",
+          ".svn/*",
+          ".hg/*",
+          "CVS/*",
+          ".next/*",
+          ".docz/*",
+          ".DS_Store",
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+            no_ignore = true,
+            find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+          },
+        },
         extensions = {
           ["ui-select"] = {
             require("telescope.themes").get_dropdown({}),
           },
         },
       })
+
       local builtin = require("telescope.builtin")
       local map = vim.keymap.set
 
