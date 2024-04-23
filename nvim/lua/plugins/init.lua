b78c1ca524e835942f65818de012f3fda26d3911
Paul van de Ven <paul@get-protocol.io>
2024-04-23 15:24:26 +0200

    format plugins/init

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -1,81 +1,81 @@
 return {
-	{
-		"catppuccin/nvim",
-		lazy = false,
-		name = "catppuccin",
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("catppuccin-mocha")
-		end,
-	},
+  {
+    "catppuccin/nvim",
+    lazy = false,
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("catppuccin-mocha")
+    end,
+  },
 
-	"nvim-lua/plenary.nvim",
+  "nvim-lua/plenary.nvim",
 
-	-- Formatting
-	{
-		"stevearc/conform.nvim",
-		event = "BufWritePre",
-		cmd = { "ConformInfo" },
-		opts = function()
-			return require("configs.conform_config")
-		end,
-		config = function(_, opts)
-			require("conform").setup(opts)
-		end,
-	},
+  -- Formatting
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    cmd = { "ConformInfo" },
+    opts = function()
+      return require("configs.conform_config")
+    end,
+    config = function(_, opts)
+      require("conform").setup(opts)
+    end,
+  },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		event = { "BufReadPost", "BufNewFile" },
-		cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-		build = ":TSUpdate",
-		opts = function()
-			return require("configs.treesitter")
-		end,
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    build = ":TSUpdate",
+    opts = function()
+      return require("configs.treesitter")
+    end,
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 
-	-- git
-	{
-		"tpope/vim-fugitive",
-		cmd = {
-			"Git",
-		},
-	},
+  -- git
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "Git",
+    },
+  },
 
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "User FilePost",
-		opts = function()
-			return require("configs.git")
-		end,
-		config = function(_, opts)
-			require("gitsigns").setup(opts)
-		end,
-	},
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      return require("configs.git")
+    end,
+    config = function(_, opts)
+      require("gitsigns").setup(opts)
+    end,
+  },
 
-	-- Telescope
-	{
-		"nvim-telescope/telescope.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"nvim-treesitter/nvim-treesitter",
-			"nvim-telescope/telescope-ui-select.nvim",
-		},
-		cmd = "Telescope",
-		opts = function()
-			return require("configs.telescope_config")
-		end,
-		config = function(_, opts)
-			local telescope = require("telescope")
-			telescope.setup(opts)
+  -- Telescope
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-telescope/telescope-ui-select.nvim",
+    },
+    cmd = "Telescope",
+    opts = function()
+      return require("configs.telescope_config")
+    end,
+    config = function(_, opts)
+      local telescope = require("telescope")
+      telescope.setup(opts)
 
-			-- load extensions
-			for _, ext in ipairs(opts.extensions) do
-				telescope.load_extension(ext)
-			end
-		end,
-	},
+      -- load extensions
+      for _, ext in ipairs(opts.extensions) do
+        telescope.load_extension(ext)
+      end
+    end,
+  },
 }
